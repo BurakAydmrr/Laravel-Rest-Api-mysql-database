07ab877e392db69d995b4a8f25f06b459ccdfccc
Burak Aydemir <aydemirburak404@gmail.com>
2024-04-07 00:00:38 +0300

    first commit

---
+++ database/migrations/2024_04_06_001347_create_items_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Ramsey\Uuid\Type\Integer;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->string("name");
+            $table->string("desc");
+            $table->integer("price");
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('items');
+    }
+};
